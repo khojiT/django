ffe8b27ad9d0aa647aadccf11e74c43fb4b2a0ce
khojiT <a.khoji2001@gmail.com>
2021-07-28 14:04:19 +0430

    add new admin panel

---
+++ SYNTHETICA_customized_admin_panel/acc/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+LEVEL_CH = (
+    ('0', 'beginner'),
+    ('1', 'junior'),
+    ('2', 'mid'),
+    ('3','senior'),
+)
+class mainUser(AbstractUser):
+    # username = models.CharField(max_length=200,unique=True,verbose_name='username')
+    age =models.IntegerField(verbose_name='Age',null=True,blank=True)
+    salary = models.IntegerField(verbose_name= 'Salary')
+    level = models.CharField(max_length=1,default='0',verbose_name='Level')
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['level','salary']
+
+    def __str__(self):
+        return self.username
